7eede6bc3e215e1a5a9427dd75319535ba626582
Vivek Bhookya <v.bhookya98@gmail.com>
2019-12-22 03:45:54 -0600

    Init, can detect shake with latency of 1 second between shakes

---
+++ app/src/main/java/us/mrvivacio/shakemaster/MainActivity.java
@@ -0,0 +1,77 @@
+package us.mrvivacio.shakemaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+
+    // Sensors
+    private Sensor accelerometer;
+
+    float x, y, z;
+    float prevX = 0, prevY = 0, prevZ = 0;
+    long prevTime = 0;
+
+    private void initSensors() {
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (accelerometer == null) {
+            Toast.makeText(this, "RIP no accelerometer", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        initSensors();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Accelerometer changed, detect shake
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
+
+        long currTime = System.currentTimeMillis();
+
+        if (currTime - prevTime > 1000) {
+            float speed = Math.abs(x + y + z - prevX - prevY - prevZ) * 10000;
+            Log.d("$$$$$", "" + speed);
+
+            if (speed > 30000) {
+                Toast.makeText(this, "Shaken", Toast.LENGTH_LONG).show();
+            }
+
+            prevX = x;
+            prevY = y;
+            prevZ = z;
+            prevTime = currTime;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
